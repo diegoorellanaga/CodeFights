56336ab12dd69e22e3bcf8c3993f7b7fb2c83efc
Diego <diegoorellanaga@gmail.com>
2017-10-23 10:59:37 -0400

    first 3 arrays

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # CodeFights
 just some code I made playing around in codefights
 
-# Code1.py
+#Time tests#
+## Code1.py
 
 This code implements a function that receives an array and returns the maximum length of a sub continuous array of this array that is a zigzag array. 
 
@@ -9,13 +10,13 @@ This code implements a function that receives an array and returns the maximum l
 
 a zigzag array is an array in which the two neighbors of each element is strictly bigger or strictly smaller.
 
-# Code2.py
+## Code2.py
 
 This code implements a function to calculate the smaller period of a periodicall sequence.
 
 Where **max_period** is the maximun period you want to look for, and **test_numb** is how many numbers of the sequence you want to test, the bigger the better but you have to make max_period+test_numb < len(sequence)
 
-# Code3.py
+## Code3.py
 
 This code implements a function to solve the Boggle game. Is just a brute force search of words in a 2d array.
 
@@ -23,4 +24,14 @@ wordBoggle is the main function and it has 2 parameters; board and words
 
 * **board:** is a 2d list containing letters.
 * **words:** is a list of words that we want to find in the board following Boggle rules.
+
+# Arrays #
+
+## Code4.py
+
+This script contains the 3 first tests for the arrays section
+
+
+
+
 
---
+++ code4.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 18 11:41:27 2017
+
+@author: diego
+"""
+
+# 1 test  
+import string
+    
+def firstDuplicate(a):
+    oldies={}
+    notfound=True
+    for i in range(len(a)):
+        try:
+            if oldies[a[i]]==a[i]:
+                notfound=False
+                return a[i]     
+        except:
+            oldies[a[i]]=a[i]
+    if notfound:
+        return -1        
+
+
+# 2 test    
+import math    
+    
+def firstNotRepeatingCharacter(s):
+    letters=list(set(s))
+   # print(letters)
+    d={}
+    b={}        
+    for i in letters:
+        d[i]=i
+        b[i]=i       
+    for i in range(len(s)):   
+        try:    
+            try:
+                del d[s[i]]
+            except:
+                del b[s[i]]
+        except:
+            pass    
+    #print(b,d)    
+    if len(b)==0:
+        return '_'       
+    else:
+        mini=math.inf
+        #print(b)
+        for i in letters:
+            try:
+                if s.index(b[i])<mini:
+                    mini=s.index(b[i])
+                    first_one=b[i]
+            except:
+                pass
+        return first_one
+
+# 3 test
+def rot4(a,i,j):
+    n=len(a) 
+    b=a[i][j]
+    c=0
+    for k in range(4):
+        c=b
+        old_i=i
+        old_j=j
+        i=old_j
+        j=n-old_i-1
+        b=a[i][j]
+        a[i][j]=c
+    return a
+    
+def rotateImage(a):
+    if len(a)>3:  
+        for j in range(len(a)-3+1):
+            for i in range(j,len(a)-1-j):
+                print(j,i)
+                a=rot4(a,j,i)
+    else:
+         for j in range(len(a)-1):
+             a=rot4(a,0,j)
+    return a   
+
+
+
+
+
+
+
+
+    
